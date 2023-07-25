a18b2bad5c4da25a280ed79be1bcc8ec07d2bf05
ait-varun <126562695+ait-varun@users.noreply.github.com>
2023-07-25 14:46:24 +0530

    updated selector states

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,8 +3,13 @@ import { Store, select } from '@ngrx/store';
 import { Router } from '@angular/router';
 import { AuthState } from '../login/state/login.reducer';
 import * as LoginActions from '../login/state/login.action';
-import { selectLoggedIn, selectUserName } from '../login/state/login.selector';
+import {
+  selectLoggedIn,
+  selectUser,
+  selectUserName,
+} from '../login/state/login.selector';
 import { Observable } from 'rxjs';
+import { User } from '../models/login';
 
 @Component({
   selector: 'app-homepage',
@@ -15,12 +20,14 @@ export class HomepageComponent implements OnInit {
   userName = '';
   isLoggedIn$: Observable<boolean>;
   userName$: Observable<any>;
+  user$: Observable<User>;
 
   constructor(
     private router: Router,
     private store: Store<{ auth: AuthState }>
   ) {
     this.isLoggedIn$ = this.store.select(selectLoggedIn);
+    this.user$ = this.store.select(selectUser);
     this.userName$ = this.store.select(selectUserName);
   }
 
--- src/app/models/login.ts
+++ src/app/models/login.ts
@@ -1,4 +1,11 @@
 export interface User {
+  id: number;
   username: string;
   password: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  image: string;
+  token: string;
 }
